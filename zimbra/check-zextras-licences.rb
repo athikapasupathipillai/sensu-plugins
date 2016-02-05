abf9a6d64568b0548e9b8170bfa439ce1bcf835f
Emeric Million <emillion@oasiswork.fr>
2016-02-05 17:08:15 +0100

    add ZeXtras licences check

---
+++ zimbra/check-zextras-licences.rb
@@ -0,0 +1,68 @@
+#!/usr/bin/env ruby
+#
+# check-zextras-licences
+#
+# DESCRIPTION:
+# Check number of Zextras licences left
+#
+# OUTPUT:
+# plain text
+#
+# PLATFORMS:
+# Linux
+#
+# DEPENDENCIES:
+# gem: sensu-plugin
+#
+# USAGE:
+# check-zextras-licences [-c CRITICAL -w WARNING]
+#
+# NOTES:
+#
+# LICENSE:
+# Oasiswork <dev@oasiswork.fr>
+# Released under the same terms as Sensu (the MIT license); see LICENSE
+# for details.
+#
+# AUTHORS:
+# Emeric MILLION <emillion@oasiswork.fr>
+
+require 'sensu-plugin/check/cli'
+require 'date'
+
+class CheckZextrasLicences < Sensu::Plugin::Check::CLI
+    option :critical,
+            short: '-c CRITICAL',
+            description: 'How many licences left before throw CRITICAL error',
+            default: 2
+    option :warning,
+            short: '-w WARNING',
+            description: 'How many licences left before throw WARING error',
+            default: 5
+
+    def run
+        licences_info = {}
+        output = `sudo -u zimbra /opt/zimbra/bin/zxsuite core getLicenseInfo`.lines
+
+        output.each { |line|
+            bits = line.split
+            if line.split(' ').length > 1
+                key = bits.first.strip
+                bits.shift
+                value = bits.join(' ')
+
+                licences_info[key] = value
+            end
+        }
+
+        licences_left = licences_info['licensedUsers'].to_i-licences_info['accountCount'].to_i
+
+        if licences_left <= config[:critical].to_i
+            critical "#{licences_left} licences left"
+        elsif licences_left <= config[:warning].to_i
+            warning "#{licences_left} licences left"
+        else
+            ok
+        end
+    end
+end
